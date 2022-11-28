21e08738f3b5bae0f4a8e81306b388360ac8792b
Matteo Medioli <31959430+matteomedioli@users.noreply.github.com>
2022-11-28 22:18:37 +0100

    First model, Trainer, utility functions

---
+++ models.py
@@ -0,0 +1,18 @@
+import torch.nn as nn
+
+class LanguageClassifierModel(nn.Module):
+
+    def __init__(self, vocab_size, embed_dim, num_class):
+        super(LanguageClassifierModel, self).__init__()
+        self.embedding = nn.EmbeddingBag(vocab_size, embed_dim, sparse=True)
+        self.fc = nn.Linear(embed_dim, num_class)
+        self.init_weights()
+
+    def init_weights(self):
+        nn.init.xavier_uniform_(self.embedding.weight)
+        nn.init.xavier_uniform_(self.fc.weight)
+        self.fc.bias.data.zero_()
+
+    def forward(self, text):
+        embedded = self.embedding(text)
+        return self.fc(embedded)
